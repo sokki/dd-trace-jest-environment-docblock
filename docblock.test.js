e65640e63741e9c25abf4f45370ef2ea6c55a071
Till <happysokki@gmail.com>
2022-03-29 12:40:29 +0000

    add repro

---
+++ docblock.test.js
@@ -0,0 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
+test('simple test', ()=> {
+    expect(1).toEqual(1)
+})
---
+++ environment.js
@@ -0,0 +1,6 @@
+if (process.env.DD_ENV === "ci") {
+    require("dd-trace/ci/jest/env");
+  }
+  
+  module.exports = require("jest-environment-node");
+  
